b728bd23f23642df2f68def12c5c9c196c12d9da
lks21c <lks21c@gmail.com>
2017-11-15 16:01:47 +0900

    print res

--- app.py
+++ app.py
@@ -43,6 +43,25 @@ def webhook():
 
     res = processRequest(req)
 
+    res = {
+                            "basicCard": {
+                                "title": "Math & prime numbers",
+                                "formattedText": "42 is an even composite number. It\n    is composed of three distinct prime numbers multiplied together. It\n    has a total of eight divisors. 42 is an abundant number, because the\n    sum of its proper divisors 54 is greater than itself. To count from\n    1 to 42 would take you about twenty-one…",
+                                "image": {
+                                    "url": "https://example.google.com/42.png",
+                                    "accessibilityText": "Image alternate text"
+                                },
+                                "buttons": [
+                                    {
+                                        "title": "Read more",
+                                        "openUrlAction": {
+                                            "url": "https://example.google.com/mathandprimes"
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+
     res = json.dumps(res, indent=4)
 
     r = make_response(res)
