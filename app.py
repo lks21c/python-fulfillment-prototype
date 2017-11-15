f584a4b54441b81cbe5dd9151002d83e48612872
lks21c <lks21c@gmail.com>
2017-11-15 16:11:19 +0900

    print res

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def webhook():
     res = processRequest(req)
 
     res = {
-        "textToSpeech": "Howdy! I can tell you fun facts about almost any number, like 42. What do you have in mind?",
+        "speech": "Howdy! I can tell you fun facts about almost any number, like 42. What do you have in mind?",
         "displayText": "Howdy! I can tell you fun facts about almost any number. What do you have in mind?"
     }
 
